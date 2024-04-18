5f976bf313bef17ab68d1683d170d55483410feb
Eoghan55555 <S00233339@atu.ie>
2024-04-18 12:13:47 +0100

    test

---
+++ SearchTesting/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+const lessons = [
+    {
+        "id": "SG_EELCO_K08/F/Y4",
+        "lName": "ifbojn",
+        "sName": "if",
+        "department":"engineering",
+        "tags": [
+            "electronics",
+            "computers"
+        ]
+
+      },
+]
